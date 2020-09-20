587357417368666e614d9312ddc92da19861a633
Adarsh Gupta <adarshgupta.cse18@itbhu.ac.in>
2020-09-20 19:30:08 +0530

    Animation part 1

--- temp.py
+++ temp.py
@@ -1,5 +1,21 @@
-for i in range(0,100):
-    print(i/100,end=" ")
+# for i in range(0,100):
+#     print(i/100,end=" ")
 
+# # for i in range(100):
+#     # print("0 0 0, 0 0 -1,",end="");
+
+#creating coords for scene1
+
+# for i in range(0,100):
+#     print(i/400,end=" ")
+# print()
 # for i in range(100):
-    # print("0 0 0, 0 0 -1,",end="");
+    # print(f"{3*i} 0 400",end=",")
+
+
+
+#
+s="0 0.5 0.6 0.7 0.8 0.9 1";
+s=list(map(float,s.split()))
+for i in s:
+    print(i/2,end=" ")
